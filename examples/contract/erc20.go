ab560973d4d7d9ada0bbc1a92440102343ff9345
Alexander <alexandersemenov1994@gmail.com>
2023-02-26 15:23:29 +0600

    module

--- examples/contract/erc20.go
+++ examples/contract/erc20.go
@@ -5,8 +5,8 @@ import (
 	"math/big"
 	"os"
 
-	"github.com/chenzhijie/go-web3"
-	"github.com/chenzhijie/go-web3/types"
+	"github.com/alexray-2016/go-web3"
+	"github.com/alexray-2016/go-web3/types"
 	"github.com/ethereum/go-ethereum/common"
 )
 
--- examples/evmchains/polygon/main.go
+++ examples/evmchains/polygon/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/chenzhijie/go-web3"
+	"github.com/alexray-2016/go-web3"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
